bfcafed287bc844943e3f8c1e62606b2f5136e41
MARIEM <tanazeftimariem9@gmail.com>
2021-12-05 14:01:52 +0100

    test function2

--- app/src/test/java/com/example/myapplication/SommeTest.java
+++ app/src/test/java/com/example/myapplication/SommeTest.java
@@ -8,8 +8,8 @@ public class SommeTest {
     @Test
     public void add() {
         Somme somme=new Somme();
-        int result = somme.add(5,2);
-        int expected = 7;
+        int result = somme.add(4,2);
+        int expected = 6;
         assertEquals(expected, result);
     }
 }
